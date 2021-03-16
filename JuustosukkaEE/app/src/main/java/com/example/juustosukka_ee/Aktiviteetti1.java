5fb92d4b4e283c1966d2003a8614d27fe136ba42
Tuomas Metsoila <tuomas.metsoila@gmail.com>
2021-03-16 15:19:32 +0200

    Toinen sivu

---
+++ JuustosukkaEE/app/src/main/java/com/example/juustosukka_ee/Aktiviteetti1.java
@@ -0,0 +1,14 @@
+package com.example.juustosukka_ee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aktiviteetti1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktiviteetti1);
+    }
+}
--- JuustosukkaEE/app/src/main/java/com/example/juustosukka_ee/MainActivity.java
+++ JuustosukkaEE/app/src/main/java/com/example/juustosukka_ee/MainActivity.java
@@ -2,20 +2,39 @@ package com.example.juustosukka_ee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAktiviteetti1();
+            }
+        });
+    }
+
+    public void openAktiviteetti1(){
+        Intent intent = new Intent(this, Aktiviteetti1.class);
+        startActivity(intent);
+    }
+
         // Moi tästä alkaa meiän projekti!!
 
         // JEE
 
         //juustonakhu
-    }
+
 
 }
