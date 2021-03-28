dc5e524d31d53b2e2b106202f7fd120eba86eaa7
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-03-28 19:51:05 +0300

    NewUserPage done!

--- JuustosukkaEE/app/src/main/java/com/example/juustosukka_ee/NewUserPage.java
+++ JuustosukkaEE/app/src/main/java/com/example/juustosukka_ee/NewUserPage.java
@@ -2,30 +2,38 @@ package com.example.juustosukka_ee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class NewUserPage extends AppCompatActivity {
-    private Button continuebutton;
+    private ImageButton returnButton;
+    private Button createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newuserpage);
 
-        continuebutton = (Button) findViewById(R.id.continuebutton);
-        continuebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openHomePage();
-            }
-        });
+        returnButton = (ImageButton) findViewById(R.id.returnbutton);
+        createAccount = (Button) findViewById(R.id.createAccount);
     }
 
-    public void openHomePage(){
-        Intent intent = new Intent(this, HomePage.class);
+    public void openLoginPage(View v){
+        Intent intent = new Intent(this, LogInPage.class);
         startActivity(intent);
+    }
+
+    public void showToast (View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Käyttäjätili luotu!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
