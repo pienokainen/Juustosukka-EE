50eb19aed68bddebca652222bcbcc2876b31a4a0
Tuomas Metsoila <tuomas.metsoila@gmail.com>
2021-04-21 13:25:56 +0300

    Tuhoa

--- JuustosukkaEE/app/src/main/java/com/example/juustosukka_ee/HomePage.java
+++ JuustosukkaEE/app/src/main/java/com/example/juustosukka_ee/HomePage.java
@@ -1,19 +1,24 @@
 package com.example.juustosukka_ee;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.data.model.User;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -21,27 +26,59 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.EntryXComparator;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+
+import static android.content.ContentValues.TAG;
+import static androidx.browser.browseractions.BrowserActionsIntent.KEY_TITLE;
+import static androidx.browser.customtabs.CustomTabsIntent.KEY_DESCRIPTION;
 
 
 public class HomePage extends AppCompatActivity {
+    private DatabaseReference mDatabase;
 
-    Button nappi;
     EditText weightentry;
-    int luku;
+    EditText stepsentry;
+    FirebaseFirestore db;
+    DocumentReference documentReference;
+
     private FirebaseAuth mAuth;
     LineChart mpstepschart;
     LineChart mpweightchart;
@@ -49,9 +86,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        setProfile();
         mAuth = FirebaseAuth.getInstance();
-        nappi = findViewById(R.id.plusnappi);
         weightentry = findViewById(R.id.weight);
+        stepsentry = findViewById(R.id.steps);
         BottomNavigationView bottomNavigationView = findViewById(R.id.navi);
         bottomNavigationView.setSelectedItemId(R.id.home);
 
@@ -74,39 +112,43 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
+
+
         mpweightchart = findViewById(R.id.weightchart);
-        mpstepschart = findViewById(R.id.stepschart);
         LineDataSet lineDataSet1 = new LineDataSet(weightData(), "Weight");
-        LineDataSet lineDataSet2 = new LineDataSet(stepsData(), "Steps");
         ArrayList<ILineDataSet> weightdata = new ArrayList<>();
-        ArrayList<ILineDataSet> stepsdata = new ArrayList<>();
         weightdata.add(lineDataSet1);
-        stepsdata.add(lineDataSet2);
         LineData data1 = new LineData(weightdata);
         mpweightchart.setData(data1);
         mpweightchart.invalidate();
         mpweightchart.setDrawBorders(true);
         mpweightchart.setBorderColor(Color.BLACK);
         mpweightchart.setBorderWidth(1);
-        //mpweightchart.setBackgroundColor(Color.CYAN); //tästä väriä taustalle jos halajaa
+
+        //mpweightchart.setBackgroundColor(Color.CYAN);
         XAxis xAxis1 = mpweightchart.getXAxis();
         YAxis yAxisr1 = mpweightchart.getAxisRight();
         YAxis yAxisl1 = mpweightchart.getAxisLeft();
+
+        mpstepschart = findViewById(R.id.stepschart);
+        LineDataSet lineDataSet2 = new LineDataSet(stepsData(), "Steps");
+        ArrayList<ILineDataSet> stepsdata = new ArrayList<>();
+        stepsdata.add(lineDataSet2);
         LineData data2 = new LineData(stepsdata);
         mpstepschart.setData(data2);
         mpstepschart.invalidate();
         mpstepschart.setDrawBorders(true);
         mpstepschart.setBorderColor(Color.BLACK);
         mpstepschart.setBorderWidth(1);
-        //mpstepschart.setBackgroundColor(Color.CYAN);
+        //mpstepschart.setBackgroundColor(Color.BLACK);
         XAxis xAxis2 = mpstepschart.getXAxis();
         xAxis1.setValueFormatter(new com.github.mikephil.charting.formatter.ValueFormatter() {
 
-            private final SimpleDateFormat mFormat = new SimpleDateFormat("dd.MM.", Locale.ENGLISH);
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public String getFormattedValue(float value) {
-                long millis = TimeUnit.DAYS.toMillis((long) value);
-                return mFormat.format(new Date(millis));
+                LocalDate date1 = LocalDate.ofEpochDay((long) value);
+                return date1.format(DateTimeFormatter.ofPattern("dd.MM."));
             }
 
 
@@ -136,64 +178,201 @@ public class HomePage extends AppCompatActivity {
 
         xAxis2.setValueFormatter(new com.github.mikephil.charting.formatter.ValueFormatter() {
 
-            private final SimpleDateFormat mFormat = new SimpleDateFormat("dd.MM.", Locale.ENGLISH);
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public String getFormattedValue(float value) {
-                long millis = TimeUnit.DAYS.toMillis((long) value);
-                return mFormat.format(new Date(millis));
+                LocalDate date1 = LocalDate.ofEpochDay((long) value);
+                return date1.format(DateTimeFormatter.ofPattern("dd.MM."));
             }
 
 
         });
-
-
     }
-
-
 
     private ArrayList<Entry> stepsData(){
         ArrayList<Entry> stepslist = new ArrayList<Entry>();
-        stepslist.add(new Entry(0,2000));
-        stepslist.add(new Entry(1,2300));
-        stepslist.add(new Entry(2,1990));
-        stepslist.add(new Entry(3,1800));
-        stepslist.add(new Entry(4,2222));
-        stepslist.add(new Entry(5,1999));
+        List<Entry> stepsEntry = getSteps();
+        stepslist.add(new Entry(0,0));
+        for (Entry c : stepsEntry){
+            float x = c.getX();
+            float y = c.getY();
+
+            stepslist.add(c);
+        }
+        stepslist.remove(0);
         return stepslist;
     }
-
     private ArrayList<Entry> weightData(){
-        ArrayList<Entry> weightlist = new ArrayList<Entry>();
-        weightlist.add(new Entry(1,29));
-        weightlist.add(new Entry(2,15));
-        weightlist.add(new Entry(3,22));
-        weightlist.add(new Entry(4,32));
-        weightlist.add(new Entry(5,25));
-        weightlist.add(new Entry(6,23));
-        weightlist.add(new Entry(7,32));
-        return weightlist;
+        ArrayList<Entry> weighlist = new ArrayList<Entry>();
+        List<Entry> weightEntry = getWeight();
+        weighlist.add(new Entry(0,0));
+        for (Entry c : weightlist.getInstance().getPaino()){
+            float x = c.getX();
+            float y = c.getY();
+
+            weighlist.add(c);
+        }
+        weighlist.remove(0);
+        return weighlist;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void Nappi(View v){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.uuuu");
-        LocalDate localDate = LocalDate.now();
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setWeightentry(View v){
+        LocalDate localDate = LocalDate.now();
+        String date = String.valueOf(localDate.toEpochDay());
         String userid = mAuth.getCurrentUser().getUid();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         Map<String, Object> docData = new HashMap<>();
         Map<String, Object> nestedData = new HashMap<>();
+
         int weightI = Integer.parseInt(weightentry.getText().toString());
-        nestedData.put(dtf.format(localDate), weightI);
+        nestedData.put(date, weightI);
         docData.put("Paino", nestedData);
-
-
         db.collection("users").document(userid)
                 .set(docData, SetOptions.merge());
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setStepsentry(View v) {
+        LocalDate localDate = LocalDate.now();
+        String date = String.valueOf(localDate.toEpochDay());
+        String userid = mAuth.getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> docData2 = new HashMap<>();
+        Map<String, Object> nestedData2 = new HashMap<>();
 
+        int stepsI = Integer.parseInt(stepsentry.getText().toString());
+        nestedData2.put(date, stepsI);
+        docData2.put("Askeleet", nestedData2);
+
+
+        db.collection("users").document(userid)
+                .set(docData2, SetOptions.merge());
+    }
+
+
+    private ArrayList<Entry> getSteps(){
+
+        String userid = mAuth.getCurrentUser().getUid();
+        FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+
+        FirebaseFirestore.getInstance().collection("users")
+                .document(userid).get()
+                .addOnCompleteListener(new
+                                               OnCompleteListener<DocumentSnapshot>() {
+                                                   @RequiresApi(api = Build.VERSION_CODES.O)
+                                                   @Override
+                                                   public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                       DocumentSnapshot document = task.getResult();
+
+                                                       Map<String, Object> map = document.getData();
+                                                       for (Map.Entry<String, Object> entry : map.entrySet()) {
+                                                           if (entry.getKey().equals("Askeleet")) {
+                                                               Log.d("TAG", entry.getValue().toString());
+                                                               System.out.println(entry.getValue().toString());
+                                                               List<String> list = new ArrayList<String>();
+                                                               list = Arrays.asList(entry.getValue().toString().split(", "));
+
+                                                               for(String s: list){
+                                                                   if (s.contains("{")){
+                                                                       s = s.replace("{", "");
+                                                                   }
+                                                                   if(s.contains("}")){
+                                                                       s = s.replace("}", "");
+                                                                   }
+                                                                   String string = s;
+                                                                   String[] parts = string.split("=");
+                                                                   //int date = Integer.parseInt(parts[0]);
+                                                                   String date = parts[0];
+                                                                   float weight = Float.parseFloat(parts[1]);
+
+                                                                   float date4 = Float.parseFloat(date);
+                                                                   weightlist.getInstance().addsteps(date4,weight);
+                                                               }
+                                                           }
+                                                       }
+
+                                                   }
+                                               });
+        return weightlist.getInstance().getSteps();
+    }
+
+
+    public void refresh(View v){
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+
+    private ArrayList<Entry> getWeight(){
+
+        String userid = mAuth.getCurrentUser().getUid();
+        FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+
+        FirebaseFirestore.getInstance().collection("users")
+            .document(userid).get()
+            .addOnCompleteListener(new
+                OnCompleteListener<DocumentSnapshot>() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+
+                        Map<String, Object> map = document.getData();
+                        try{
+                            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                                if (entry.getKey().equals("Paino")) {
+                                    Log.d("TAG", entry.getValue().toString());
+                                    System.out.println(entry.getValue().toString());
+                                    List<String> list = new ArrayList<String>();
+                                    list = Arrays.asList(entry.getValue().toString().split(", "));
+
+                                    for (String s : list) {
+                                        if (s.contains("{")) {
+                                            System.out.println(s);
+                                            s = s.replace("{", "");
+                                            System.out.println(s);
+                                        }
+                                        if (s.contains("}")) {
+                                            System.out.println(s);
+                                            s = s.replace("}", "");
+                                        }
+                                        System.out.println(s);
+                                        String string = s;
+                                        String[] parts = string.split("=");
+                                        //int date = Integer.parseInt(parts[0]);
+                                        String date = parts[0];
+                                        float weight = Float.parseFloat(parts[1]);
+
+                                        float date4 = Float.parseFloat(date);
+                                        System.out.println("Oi onnen " + date + " ja " + weight + " päivää" + date4);
+                                        weightlist.getInstance().add(date4, weight);
+                                    }
+                                }
+                            }
+                        } catch (NumberFormatException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+        return weightlist.getInstance().getPaino();
+    }
+
+    public void setProfile(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String userid = Objects.requireNonNull(auth.getCurrentUser()).getUid();
+        System.out.println(userid);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> docData = new HashMap<>();
+        Map<String, Object> nestedData = new HashMap<>();
+        nestedData.put("personal", "database");
+        docData.put("USER", nestedData);
+        db.collection("users").document(userid)
+                .set(docData, SetOptions.merge());
+
+    }
 
 }
